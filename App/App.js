3f229817d4eea2b679f70653bb9eb94437d58875
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-04-20 10:17:45 +0700

    Remove git

--- App/App.js
+++ App/App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cmpression = require("compression");
 const config = require("../config.json");
 var path = require("path");
@@ -11,14 +12,14 @@ const axios = require("axios");
 ("express-validator");
 app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 global.client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: { headless: true },
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: true },
 });
 
 global.authed = false;
@@ -27,50 +28,50 @@ const Route = require("../Routes/Routes");
 const Cors = require("cors");
 //CORS
 var whitelist = [
-  "https://webdokter.herokuapp.com",
-  "http://localhost:3000",
-  "https://api-dokter.herokuapp.com",
+    "https://webdokter.herokuapp.com",
+    "http://localhost:3000",
+    "https://api-dokter.herokuapp.com",
 ];
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
 };
 
 app.use(Cors());
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // res.setHeader(
-  //   "Access-Control-Allow-Origin",
-  //   "https://webdokter.herokuapp.com"
-  // );
-  const origin = req.headers.origin;
-  if (whitelist.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    // res.setHeader(
+    //   "Access-Control-Allow-Origin",
+    //   "https://webdokter.herokuapp.com"
+    // );
+    const origin = req.headers.origin;
+    if (whitelist.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
+    // Pass to next layer of middleware
+    next();
 });
 
 //Make body readable
@@ -85,51 +86,51 @@ app.use(Middleware);
 Route(app);
 
 app.get("/", (req, res) => {
-  res.send("Api kasir");
+    res.send("Api kasir");
 });
 
 //Otp Whatsapp
 
 client.on("qr", (qr) => {
-  console.log("qr");
-  fs.writeFileSync("./Component/last.qr", qr);
+    console.log("qr");
+    fs.writeFileSync("./Component/last.qr", qr);
 });
 
 client.on("authenticated", () => {
-  console.log("AUTH!");
-  authed = true;
+    console.log("AUTH!");
+    authed = true;
 
-  try {
-    fs.unlinkSync("./Component/last.qr");
-  } catch (err) {}
+    try {
+        fs.unlinkSync("./Component/last.qr");
+    } catch (err) {}
 });
 
 client.on("auth_failure", () => {
-  console.log("AUTH Failed !");
-  process.exit();
+    console.log("AUTH Failed !");
+    process.exit();
 });
 
 client.on("ready", () => {
-  console.log("Client is ready!");
+    console.log("Client is ready!");
 });
 
-client.on("message", async (msg) => {
-  if (config.webhook.enabled) {
-    if (msg.hasMedia) {
-      const attachmentData = await msg.downloadMedia();
-      msg.attachmentData = attachmentData;
+client.on("message", async(msg) => {
+    if (config.webhook.enabled) {
+        if (msg.hasMedia) {
+            const attachmentData = await msg.downloadMedia();
+            msg.attachmentData = attachmentData;
+        }
+        axios.post(config.webhook.path, { msg });
     }
-    axios.post(config.webhook.path, { msg });
-  }
 });
 client.on("disconnected", () => {
-  console.log("disconnected");
+    console.log("disconnected");
 });
 client.initialize();
 
-app.use(function (req, res, next) {
-  console.log(req.method + " : " + req.path);
-  next();
+app.use(function(req, res, next) {
+    console.log(req.method + " : " + req.path);
+    next();
 });
 
 module.exports = app;
--- Util/Database.js
+++ Util/Database.js
@@ -6,9 +6,7 @@ var options = {
 };
 
 var pgp = require("pg-promise")(options);
-// var connectionString =
-//   "postgres://ogkhatpkhgdein:48ef020621fda4972802a48bee243d962ca8bd11e04029019b8111c0e39aa626@ec2-34-195-143-54.compute-1.amazonaws.com:5432/d2ke523leggc54";
-require("dotenv").config();
+
 let ssl = { rejectUnauthorized: false };
 const config = {
     connectionString: process.env.CONNECTION_STRING,
