dd488903b244532670b9fad4f4d9e3eb64265424
YappierUwU <87288148+YappierUwU@users.noreply.github.com>
2022-04-20 10:32:48 +0700

    Tambah Komentar

--- public/index.js
+++ public/index.js
@@ -4,5 +4,6 @@ const server = require("http").createServer();
 const port = process.env.PORT || 1000;
 
 app.listen(port, () => {
-    console.log(`REST at http://localhost:${port}`);
+  // test
+  console.log(`REST at http://localhost:${port}`);
 });
