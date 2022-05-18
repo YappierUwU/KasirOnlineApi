e5c51fbfb0246405422410a422bb3ff2287a31b6
YappierUwU <87288148+YappierUwU@users.noreply.github.com>
2022-05-18 10:10:54 +0700

    menambahkan username

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -7,57 +7,58 @@ const validate = require("../Validation/UserValidation");
 const handlerInput = require("../Util/ValidationHandler");
 // twlp
 // nama pemilik nama usaha lokasui usaha jenis usaha
-router.post("/register", validate(), handlerInput, async function(req, res) {
-    let sql = `INSERT INTO tbltoko (nama_toko, alamat_toko, nomer_toko, nama_pemilik, email_toko, password_toko, jenis_toko) VALUES ( NULL, NULL, NULL, NULL, $5, $6 , NULL ) returning idtoko`;
-    let data = [
-        "-",
-        "-",
-        "-",
-        "-",
-        req.body.email,
-        encrypt(req.body.password),
-        "-",
-    ];
-    const toko = await koneksi.oneOrNone(sql, data);
-    let token = generate(toko.idtoko, "2");
-    res.status(200).json({
-        status: true,
-        data: {
-            email: req.body.email,
-        },
-        token: token,
-    });
+router.post("/register", validate(), handlerInput, async function (req, res) {
+  let sql = `INSERT INTO tbltoko (nama_toko, alamat_toko, nomer_toko, nama_pemilik, email_toko, password_toko, jenis_toko) VALUES ( NULL, NULL, NULL, NULL, $5, $6 , NULL ) returning idtoko`;
+  let data = [
+    "-",
+    "-",
+    "-",
+    "-",
+    req.body.email,
+    encrypt(req.body.password),
+    "-",
+  ];
+  const toko = await koneksi.oneOrNone(sql, data);
+  let token = generate(toko.idtoko, "2");
+  res.status(200).json({
+    status: true,
+    data: {
+      email: req.body.email,
+    },
+    token: token,
+  });
 });
 
-router.post("/login", async function(req, res, next) {
-    let sql = `SELECT nomer_toko, nama_toko,idtoko,email_toko FROM tbltoko where email_toko=$1 and password_toko=$2`;
-    let sqlOTP = "select idotp from tblotp where idtoko=$1;";
+router.post("/login", async function (req, res, next) {
+  let sql = `SELECT nomer_toko, nama_toko,nama_pemilik,idtoko,email_toko FROM tbltoko where email_toko=$1 and password_toko=$2`;
+  let sqlOTP = "select idotp from tblotp where idtoko=$1;";
 
-    let data = [req.body.email, encrypt(req.body.password)];
-    let result = await koneksi.any(sql, data);
+  let data = [req.body.email, encrypt(req.body.password)];
+  let result = await koneksi.any(sql, data);
 
-    if (result.length > 0) {
-        let otp = await koneksi.any(sqlOTP, [result[0].idtoko]);
-        console.log(otp);
-        let { nomer_toko, nama_toko } = result[0];
-        let page = "Dashboard";
-        if (!nomer_toko || otp.length > 0) {
-            page = "Verifikasi OTP";
-        } else if (nomer_toko && !nama_toko) {
-            page = "Ubah Data Toko";
-        }
-
-        let token = generate(result[0].idtoko, "2");
-        res.json({
-            token: token,
-            page: page,
-            data: {
-                email: result[0].email_toko,
-            },
-        });
-    } else {
-        res.status(404).json({ msg: "Email atau Password tidak ditemukan" });
+  if (result.length > 0) {
+    let otp = await koneksi.any(sqlOTP, [result[0].idtoko]);
+    console.log(otp);
+    let { nomer_toko, nama_toko } = result[0];
+    let page = "Dashboard";
+    if (!nomer_toko || otp.length > 0) {
+      page = "Verifikasi OTP";
+    } else if (nomer_toko && !nama_toko) {
+      page = "Ubah Data Toko";
     }
+
+    let token = generate(result[0].idtoko, "2");
+    res.json({
+      token: token,
+      page: page,
+      data: {
+        username: result[0].nama_pemilik,
+        email: result[0].email_toko,
+      },
+    });
+  } else {
+    res.status(404).json({ msg: "Email atau Password tidak ditemukan" });
+  }
 });
 
 module.exports = router;
