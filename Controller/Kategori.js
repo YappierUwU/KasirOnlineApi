fb3627627dc6444add63816b61b9526e8d78de6e
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-06-10 08:57:01 +0700

    cari kategori satuan

--- Controller/Kategori.js
+++ Controller/Kategori.js
@@ -3,116 +3,114 @@ var router = express.Router();
 var koneksi = require("../Util/Database");
 const handlerInput = require("../Util/ValidationHandler");
 const validate = require("../Validation/KategoriValidation");
-router.get("/", async function (req, res, next) {
-  const { timestamp = false } = req.query;
-  let sql,
-    result = [];
-  if (timestamp) {
-    sql =
-      "select * from tblkategori where idtoko=$1 and (created_at > $2 or updated_at > $2) order by nama_kategori";
-    result = await koneksi.query(sql, [req.context.idtoko, timestamp]);
-  } else {
-    sql = "select * from tblkategori where idtoko=$1 order by nama_kategori";
-    result = await koneksi.query(sql, [req.context.idtoko]);
-  }
+router.get("/", async function(req, res, next) {
+    const { timestamp = false, cari = "" } = req.query;
+    let sql,
+        result = [];
+    if (timestamp) {
+        sql = `select * from tblkategori where idtoko=$1 and nama_kategori ilike '%${cari}%'  and (created_at > $2 or updated_at > $2) order by nama_kategori`;
+        result = await koneksi.query(sql, [req.context.idtoko, timestamp]);
+    } else {
+        sql = `select * from tblkategori where idtoko=$1 and nama_kategori ilike '%${cari}%' order by nama_kategori`;
+        result = await koneksi.query(sql, [req.context.idtoko]);
+    }
 
-  if (result.length > 0) {
-    res.status(200).json({
-      status: true,
-      data: result,
-    });
-  } else {
-    res.status(200).json({
-      status: true,
-      data: [],
-    });
-  }
-  //
+    if (result.length > 0) {
+        res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } else {
+        res.status(200).json({
+            status: true,
+            data: [],
+        });
+    }
+    //
 });
 
-router.get("/:id", async function (req, res, next) {
-  let id = req.params.id;
-  let result = await koneksi.oneOrNone(
-    "select * from tblkategori where idkategori = $1 and idtoko=$2",
-    [id, req.context.idtoko]
-  );
+router.get("/:id", async function(req, res, next) {
+    let id = req.params.id;
+    let result = await koneksi.oneOrNone(
+        "select * from tblkategori where idkategori = $1 and idtoko=$2", [id, req.context.idtoko]
+    );
 
-  if (result) {
-    res.status(200).json({
-      status: true,
-      data: result,
-    });
-  } else {
-    res.status(400).json({
-      status: false,
-      data: {},
-      message: "Data tidak ditemukan",
-    });
-  }
-  //
+    if (result) {
+        res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } else {
+        res.status(400).json({
+            status: false,
+            data: {},
+            message: "Data tidak ditemukan",
+        });
+    }
+    //
 });
 
-router.post("/", validate(), handlerInput, function (req, res, next) {
-  let sql = `INSERT INTO tblkategori (nama_kategori, idtoko) VALUES ($1,$2) returning *`;
-  let data = [req.body.nama_kategori, req.context.idtoko];
+router.post("/", validate(), handlerInput, function(req, res, next) {
+    let sql = `INSERT INTO tblkategori (nama_kategori, idtoko) VALUES ($1,$2) returning *`;
+    let data = [req.body.nama_kategori, req.context.idtoko];
 
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        status: false,
-        data: {},
-        message: err.message,
-      });
-    });
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((err) => {
+            res.status(400).json({
+                status: false,
+                data: {},
+                message: err.message,
+            });
+        });
 });
 
-router.post("/:id", validate(), handlerInput, function (req, res) {
-  let idkategori = req.params.id;
-  let sql = `UPDATE tblkategori SET nama_kategori=$1, idtoko=$2 WHERE idkategori=$3 returning *`;
-  let data = [req.body.nama_kategori, req.context.idtoko, idkategori];
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((e) => {
-      res.status(400).json({
-        status: false,
-        data: {},
-        message: e.message,
-      });
-    });
+router.post("/:id", validate(), handlerInput, function(req, res) {
+    let idkategori = req.params.id;
+    let sql = `UPDATE tblkategori SET nama_kategori=$1, idtoko=$2 WHERE idkategori=$3 returning *`;
+    let data = [req.body.nama_kategori, req.context.idtoko, idkategori];
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((e) => {
+            res.status(400).json({
+                status: false,
+                data: {},
+                message: e.message,
+            });
+        });
 });
 
-router.delete("/:id", async function (req, res, next) {
-  let id = req.params.id;
-  let sql = `DELETE FROM tblkategori WHERE idkategori=$1 returning *`;
-  let data = [id];
+router.delete("/:id", async function(req, res, next) {
+    let id = req.params.id;
+    let sql = `DELETE FROM tblkategori WHERE idkategori=$1 returning *`;
+    let data = [id];
 
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        status: false,
-        data: {},
-        message: err.message,
-      });
-    });
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((err) => {
+            res.status(400).json({
+                status: false,
+                data: {},
+                message: err.message,
+            });
+        });
 });
 module.exports = router;
--- Controller/Satuan.js
+++ Controller/Satuan.js
@@ -3,117 +3,115 @@ var router = express.Router();
 var koneksi = require("../Util/Database");
 const handlerInput = require("../Util/ValidationHandler");
 const validate = require("../Validation/SatuanValidation");
-router.get("/", async function (req, res, next) {
-  const { timestamp = false } = req.query;
-  let result = [];
-  if (timestamp) {
-    result = await koneksi.query(
-      "select * from tblsatuan where (created_at > $2 or updated_at > $2) and idtoko=$1",
-      [req.context.idtoko, timestamp]
+router.get("/", async function(req, res, next) {
+    const { timestamp = false, cari = "" } = req.query;
+    let result = [];
+    if (timestamp) {
+        result = await koneksi.query(
+            `select * from tblsatuan where  nama_satuan ilike '%${cari}%' and (created_at > $2 or updated_at > $2) and idtoko=$1`, [req.context.idtoko, timestamp]
+        );
+    } else {
+        result = await koneksi.query(
+            `select * from tblsatuan where nama_satuan ilike '%${cari}%' and idtoko=$1`, [req.context.idtoko]
+        );
+    }
+
+    if (result.length > 0) {
+        res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } else {
+        res.status(200).json({
+            status: true,
+            data: [],
+        });
+    }
+    //
+});
+
+router.get("/:id", async function(req, res, next) {
+    let id = req.params.id;
+    let result = await koneksi.oneOrNone(
+        "select * from tblsatuan where idsatuan = $1", [id]
     );
-  } else {
-    result = await koneksi.query("select * from tblsatuan where idtoko=$1", [
-      req.context.idtoko,
-    ]);
-  }
 
-  if (result.length > 0) {
-    res.status(200).json({
-      status: true,
-      data: result,
-    });
-  } else {
-    res.status(200).json({
-      status: true,
-      data: [],
-    });
-  }
-  //
+    if (result) {
+        res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } else {
+        res.status(304).json({
+            status: false,
+            data: {},
+            message: "Data tidak ditemukan",
+        });
+    }
+    //
 });
 
-router.get("/:id", async function (req, res, next) {
-  let id = req.params.id;
-  let result = await koneksi.oneOrNone(
-    "select * from tblsatuan where idsatuan = $1",
-    [id]
-  );
+router.post("/", validate(), handlerInput, function(req, res, next) {
+    let sql = `INSERT INTO tblsatuan (nama_satuan, idtoko) VALUES ($1,$2) returning *`;
+    let data = [req.body.nama_satuan, req.context.idtoko];
 
-  if (result) {
-    res.status(200).json({
-      status: true,
-      data: result,
-    });
-  } else {
-    res.status(304).json({
-      status: false,
-      data: {},
-      message: "Data tidak ditemukan",
-    });
-  }
-  //
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((e) => {
+            res.status(304).json({
+                status: false,
+                data: {},
+                message: e.message,
+            });
+        });
 });
 
-router.post("/", validate(), handlerInput, function (req, res, next) {
-  let sql = `INSERT INTO tblsatuan (nama_satuan, idtoko) VALUES ($1,$2) returning *`;
-  let data = [req.body.nama_satuan, req.context.idtoko];
-
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((e) => {
-      res.status(304).json({
-        status: false,
-        data: {},
-        message: e.message,
-      });
-    });
+router.post("/:id", validate(), handlerInput, function(req, res) {
+    let idsatuan = req.params.id;
+    let sql = `UPDATE tblsatuan SET nama_satuan=$1, idtoko=$2 WHERE idsatuan=$3 returning *`;
+    let data = [req.body.nama_satuan, req.context.idtoko, idsatuan];
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((e) => {
+            res.status(304).json({
+                status: false,
+                data: {},
+                message: e.message,
+            });
+        });
 });
 
-router.post("/:id", validate(), handlerInput, function (req, res) {
-  let idsatuan = req.params.id;
-  let sql = `UPDATE tblsatuan SET nama_satuan=$1, idtoko=$2 WHERE idsatuan=$3 returning *`;
-  let data = [req.body.nama_satuan, req.context.idtoko, idsatuan];
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((e) => {
-      res.status(304).json({
-        status: false,
-        data: {},
-        message: e.message,
-      });
-    });
-});
+router.delete("/:id", async function(req, res, next) {
+    let id = req.params.id;
+    let sql = `DELETE FROM tblsatuan WHERE idsatuan=$1 returning *`;
+    let data = [id];
 
-router.delete("/:id", async function (req, res, next) {
-  let id = req.params.id;
-  let sql = `DELETE FROM tblsatuan WHERE idsatuan=$1 returning *`;
-  let data = [id];
-
-  koneksi
-    .oneOrNone(sql, data)
-    .then((result) => {
-      res.status(200).json({
-        status: true,
-        data: result,
-      });
-    })
-    .catch((err) => {
-      res.status(304).json({
-        status: false,
-        data: {},
-        message: err.message,
-      });
-    });
+    koneksi
+        .oneOrNone(sql, data)
+        .then((result) => {
+            res.status(200).json({
+                status: true,
+                data: result,
+            });
+        })
+        .catch((err) => {
+            res.status(304).json({
+                status: false,
+                data: {},
+                message: err.message,
+            });
+        });
 });
 module.exports = router;
