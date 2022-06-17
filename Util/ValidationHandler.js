72bced10c5c435ff2eadf0b23325eef0dc8bf7bb
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-06-17 08:43:51 +0700

    test

--- Util/ValidationHandler.js
+++ Util/ValidationHandler.js
@@ -12,8 +12,7 @@ const resultValidation = validationResult.withDefaults({
 function handlerInput(req, res, next) {
     let error = resultValidation(req);
     if (!error.isEmpty()) {
-        const message = error[0].msg;
-        res.status(406).json({ status: false, message });
+        res.status(406).json({ status: false, error: error.array() });
     } else {
         next();
     }
